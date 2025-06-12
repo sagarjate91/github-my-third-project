875c83f5c181ecb63d5008077259b5272985ae44
Sagar Jate <jate.sagar91@gmail.com>
2025-06-12 10:28:09 +0530

    My First commit from vs code

---
+++ src/main/java/com/github/github_my_third_project/GithubMyThirdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.github_my_third_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubMyThirdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubMyThirdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/github_my_third_project/GithubMyThirdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.github_my_third_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubMyThirdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
